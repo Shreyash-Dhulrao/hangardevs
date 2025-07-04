5ee051380f8495871f4cb254d3e24ea8bf4abc29
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2025-07-04 16:32:22 +0530

    updates

--- src/pages/navbar/navbar.jsx
+++ src/pages/navbar/navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
     return (
         <>
             {/* Responsive Navbar */}
-            <nav className="font-montserrat fixed top-0 w-screen z-30">
+            <nav className="font-montserrat fixed top-0 w-screen z-50">
                 <div className="w-full flex justify-center items-center py-3 px-4 md:px-6 transition duration-300">
                     <div
                         ref={navInnerRef}
@@ -102,7 +102,7 @@ const Navbar = () => {
             </nav>
 
             {/* Bottom Navbar for Mobile */}
-            <nav className="md:hidden w-full fixed bottom-0 left-0 right-0 bg-white dark:bg-zinc-900 border-t border-gray-200 shadow-t z-50">
+            <nav className="md:hidden w-screen fixed bottom-0 left-0 right-0 bg-white dark:bg-zinc-900 border-t border-gray-200 shadow-t z-50">
                 <div className="flex justify-around items-center px-4 py-2">
                     <Link to="/" className="flex flex-col items-center justify-center text-xs text-gray-600 dark:text-gray-300 hover:text-blue-600 w-1/4">
                         <FaHouse className="w-5 h-5 mb-1" />
